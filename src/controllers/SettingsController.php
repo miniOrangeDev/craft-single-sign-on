d505d5ae1b7c2c30af32d05269bf2babf2ac9e3c
miniOrange <info@miniorange.com>
2022-11-15 15:56:21 +0530

    Update settings

--- src/controllers/SettingsController.php
+++ src/controllers/SettingsController.php
@@ -52,7 +52,7 @@ class SettingsController extends Controller
      *         The actions must be in 'kebab-case'
      * @access protected
      */
-    protected array|int|bool $allowAnonymous = ['index', 'check', 'delete', 'save', 'oauthsettings', 'oauthattribute', 'samlprovider', 'samlsettings', 'samlattribute', 'customsettings'];
+    protected array|int|bool $allowAnonymous = ['index', 'check', 'delete', 'save', 'deactivation', 'message', 'oauthsettings', 'oauthattribute', 'samlprovider', 'samlsettings', 'samlattribute', 'customsettings'];
 
     // Public Methods
     // =========================================================================
@@ -268,6 +268,170 @@ class SettingsController extends Controller
         $this->setSuccessFlash(Craft::t('craft-single-sign-on', 'Settings saved.'));
         $this->redirectToPostedUrl($setting);
         return $this->asJson(['success' => true]);
+    }
+
+    public function actionDeactivation(): ?string
+    {
+
+            echo $var = '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+            <style>
+                body {font-family: Arial, Helvetica, sans-serif;}
+                .modal {
+                    position: fixed; /* Stay in place */
+                    z-index: 1; /* Sit on top */
+                    padding-top: 100px; /* Location of the box */
+                    left: 0;
+                    top: 0;
+                    width: 100%; /* Full width */
+                    height: 100%; /* Full height */
+                    overflow: auto; /* Enable scroll if needed */
+                    background-color: rgb(0,0,0); /* Fallback color */
+                    background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+                }
+                
+                /* Modal Content */
+                .modal-content {
+                    position: relative;
+                    background-color: #fefefe;
+                    margin: auto;
+                    padding: 40px;
+                    border: 1px solid #888;
+                    width: 30%;
+                    min-height: 48%;
+                    border-radius: 10px;
+                    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19);
+                    -webkit-animation-name: animatetop;
+                    -webkit-animation-duration: 0.4s;
+                    animation-name: animatetop;
+                    animation-duration: 0.4s
+                }
+                
+                .modal-header .modal-footer .modal-body {
+                    padding: 2px 16px;
+                } 
+
+                .save {
+                    position: absolute;
+                    bottom: 2em;
+                    right: 3em;
+                    background-color: #4CAF50; /* Green */
+                    border: none;
+                    border-radius: 10px;
+                    color: white;
+                    padding: 20px;
+                    text-decoration: none;
+                    font-size: 16px;
+                    margin: 4px 2px;
+                    cursor: pointer;
+                }
+
+            </style>
+
+            <div class="modal">
+                <div class="modal-content">
+                <form action="" method="get" id="feedbackform">
+                    <div class="modal-header">
+                        <h2>miniOrange Single Sign On Feedback</h2>
+                    </div>
+                    <div class="modal-body">
+                        <input type="checkbox" id="nowork" name="query" value="Not working">
+                        <label for="nowork"> Not Working</label><br><br>
+                        <input type="checkbox" id="noidp" name="query" value="Looking for different IDP">
+                        <label for="noidp"> Looking for Different IDP</label><br><br>
+                        <input type="checkbox" id="noconfig" name="query" value="Difficulty in Configuring SSO">
+                        <label for="noconfig"> difficulty in configuring SSO</label><br>
+                        <h4>Tell us how we can improve</h4>
+                        <textarea id="message" name="message" rows="6" cols="70"></textarea>
+                    </div>
+                    <div class="modal-footer">
+                        <button onclick="myFunction()" class="save">Submit</button>
+                        <button onclick="document.location.reload(true)" class="save" style="right: 9em; background-color: #f44336;">Skip & Continue</button>
+                    </div>
+                </form>
+                </div>
+            </div>
+            <script>
+                function myFunction() {
+                    var form = $("#feedbackform");
+                    return $.ajax({
+                        url: "'.Craft::$app->sites->primarySite->baseUrl.'/mosinglesignon/message",
+                        data: form.serialize(),
+                        success: function(data) {
+                            window.location.href = window.location.href
+                        }
+                    });
+                }
+            </script>'; 
+
+            $cookie_name = "mo_feedback";
+            $cookie_value = "submitted";
+            setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
+
+            if(!isset($_COOKIE[$cookie_name])) {
+                exit;
+            } else {
+                return 1;
+            }
+            
+    }
+
+    public function actionMessage() 
+    {
+        $plugin = Craft::$app->plugins->getPlugin('craft-single-sign-on', false);
+        $user_info = User::find()->admin()->one();
+        $query_sub = @$_REQUEST['query'] ?: "Not Selected";
+        $feedback = @$_REQUEST['message'] ?: "No Feedback";
+
+        $ch = curl_init('https://login.xecurify.com/moas/rest/mobile/get-timestamp');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, array());
+        $currentTimeInMillis = curl_exec($ch);
+        curl_close($ch);
+
+        $customerKey = "16555";
+        $apiKey = "fFd2XcvTGDemZvbw1bcUesNJWEqKbbUq";
+        $query = " Craft SSO Plugin Installation : $user_info->email";
+        $content = '<div> Hello, <br><br>First Name :
+								<br><br>Company : '.$_SERVER['HTTP_HOST'].'
+								<br><br>Email : <a href="mailto:'.$user_info->email.'" target="_blank">'.$user_info->email.'</a>
+                                <br><br>Version : '.$plugin->version.'
+                                <br><br>Query : '.$query_sub.'
+                                <br><br>Feedback : '.$feedback.'</div>';
+                                
+        $fields = array(
+            'customerKey'	=> $customerKey,
+            'sendEmail' 	=> true,
+            'email' 		=> array(
+                'customerKey' 	=> $customerKey,
+                'fromEmail' 	=> $user_info->email,
+                'bccEmail' 		=> 'pranit@xecurify.com',
+                'fromName' 		=> 'miniOrange',
+                'toEmail' 		=> 'pranit@xecurify.com',
+                'toName' 		=> 'pranit@xecurify.com',
+                'subject' 		=> $query,
+                'content' 		=> $content
+            ),
+        );
+        
+        $stringToHash = $customerKey .  $currentTimeInMillis . $apiKey;
+        $hashValue = hash("sha512", $stringToHash);
+        $field_string = json_encode($fields);
+        
+        $ch = curl_init('https://login.xecurify.com/moas/api/notify/send');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $field_string);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            "Content-Type: application/json",
+            "Customer-Key: ".$customerKey,
+            "Timestamp: ".$currentTimeInMillis,
+            "Authorization: ".$hashValue
+            )
+        ); 
+        curl_exec($ch);
+        curl_close($ch);
+        return 1;
     }
 
 }
